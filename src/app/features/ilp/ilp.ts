4ba83d24c64bf9bd4a9d18a049dd6d6d813ca079
issa23i <isabelpastorlopezcv@gmail.com>
2026-06-21 16:04:59 +0200

    feat: ajustes sección ilp

--- src/app/features/ilp/ilp.ts
+++ src/app/features/ilp/ilp.ts
@@ -7,8 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './ilp.scss',
 })
 export class Ilp {
-  protected readonly heroImage = 'assets/redva/ilp-hero.png';
+  protected readonly heroImage = 'assets/redva/ilp_registro.jpg';
   protected readonly urbanImage = 'assets/redva/ilp-urban.png';
   protected readonly communityImage = 'assets/redva/ilp-community.png';
-  protected readonly collaborators = ['FACUA', 'CCOO', 'UGT', 'Andalucia'];
+  protected readonly collaborators = [
+    {
+      image: 'assets/redva/facuaand.png',
+      alt: 'Logo de FACUA Andalucia Consumidores en Accion',
+      url: 'https://facua.org/noticias/facua-andalucia-registra-en-el-parlamento-junto-a-otras-organizaciones-sociales-la-ilp-para-garantizar-el-derecho-efectivo-a-la-vivienda/',
+    },
+    {
+      image: 'assets/redva/ccooand.png',
+      alt: 'Logo de CCOO Andalucia',
+      url: 'https://andalucia.ccoo.es/noticia:756029',
+    },
+    {
+      image: 'assets/redva/ugtand.png',
+      alt: 'Logo de UGT Andalucia',
+      url: 'https://www.ugt-andalucia.com/w/ugt-andalucia-y-organizaciones-sociales-registran-en-el-parlamento-una-ilp-para-garantizar-el-derecho-a-la-vivienda',
+    },
+  ];
 }
