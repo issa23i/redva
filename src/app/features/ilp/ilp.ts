8ec80e5957f435d821f03f41fc62f37df9b95aec
issa23i <isabelpastorlopezcv@gmail.com>
2026-06-23 18:43:47 +0200

    feat: corregir faltas ortográficas y añadir texto y ley en pdf

--- src/app/features/ilp/ilp.ts
+++ src/app/features/ilp/ilp.ts
@@ -13,18 +13,23 @@ export class Ilp {
   protected readonly collaborators = [
     {
       image: 'assets/redva/facuaand.png',
-      alt: 'Logo de FACUA Andalucia Consumidores en Accion',
+      alt: 'Logo de FACUA Andalucía Consumidores en Acción',
       url: 'https://facua.org/noticias/facua-andalucia-registra-en-el-parlamento-junto-a-otras-organizaciones-sociales-la-ilp-para-garantizar-el-derecho-efectivo-a-la-vivienda/',
     },
     {
       image: 'assets/redva/ccooand.png',
-      alt: 'Logo de CCOO Andalucia',
+      alt: 'Logo de CCOO Andalucía',
       url: 'https://andalucia.ccoo.es/noticia:756029',
     },
     {
       image: 'assets/redva/ugtand.png',
-      alt: 'Logo de UGT Andalucia',
+      alt: 'Logo de UGT Andalucía',
       url: 'https://www.ugt-andalucia.com/w/ugt-andalucia-y-organizaciones-sociales-registran-en-el-parlamento-una-ilp-para-garantizar-el-derecho-a-la-vivienda',
+    },
+    {
+      image: 'assets/redva/logo_pah.jpg',
+      alt: 'Logo de la Plataforma de Afectadas por la Hipoteca',
+      url: 'https://afectadosporlahipoteca.com/',
     },
   ];
 }
